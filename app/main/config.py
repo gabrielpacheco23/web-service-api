6481f8f8b9911b2faeb1be92a3def5d92767c8dc
gabrielpacheco23 <gabrielpacheco23@live.com>
2018-08-16 13:53:15 -0300

    Initial commit

---
+++ app/main/config.py
@@ -0,0 +1,30 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+	SECRET_KEY = os.getenv('SECRET_KEY', 'my_precious_secret_key')
+	DEBUG = False
+
+class DevelopmentConfig(Config):
+	DEBUG = True
+	SQLALCHEMY_DATABASE_URI = 'mysql://root:1234@localhost:3306/ws_main'
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class TestingConfig(Config):
+	DEBUG = True
+	TESTING = True
+	SQLALCHEMY_DATABASE_URI = 'mysql://root:1234@localhost:3306/ws_test'
+	PRESERVE_CONTEXT_ON_EXCEPTION = False
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class ProductionConfig(Config):
+	DEBUG = False
+
+config_by_name = dict(
+	dev=DevelopmentConfig,
+	test=TestingConfig,
+	prod=ProductionConfig
+)
+
+key = Config.SECRET_KEY
