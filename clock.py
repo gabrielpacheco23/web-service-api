80c1f8595509473de95fabddd66f34ee1df9bf14
gabrielpacheco23 <gabrielpacheco23@live.com>
2018-08-21 04:34:48 -0300

    clock fix

--- clock.py
+++ clock.py
@@ -6,7 +6,7 @@ sched = BlockingScheduler()
 @sched.scheduled_job('interval', minutes=1)
 def timed_job():
     #print('This job is run every three minutes.')
-    os.execl('manage.py', '')
+    os.execv('manage.py', [' '])
 
 #@sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
 #def scheduled_job():
